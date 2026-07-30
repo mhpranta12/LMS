f248241d245d5068fcf53a016f64dbd01b9cd772
mhpranta12 <mhpranta2@gmail.com>
2026-07-31 00:57:20 +0600

    Blank Dto class addition

---
+++ Backend/LMS.API/LMS.Application/Dtos/AccountDtos/LoginResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.Dtos.AccountDtos
+{
+    public class LoginResponseDto
+    {
+    }
+}
